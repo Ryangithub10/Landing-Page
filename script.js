755ede05cc831d12504a6aae2b7745d4ff5d55a3
Ryangithub10 <technogamers543@gmail.com>
2022-06-11 21:33:00 +0800

    continuing development of dropdown (still unfinished)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const linkContent = document.getElementById('link-content');
+const nav = document.getElementById('nav');
+const btn = document.getElementById('button');
 
 hamburgerMenu.addEventListener('click', () => {
   hamburgerMenu.classList.toggle('exit');
+  hamburgerMenu.classList.toggle('change-color');
+  linkContent.classList.toggle('active');
+  nav.classList.toggle("change-bg-color");
+  btn.classList.toggle('change-color');
 });
